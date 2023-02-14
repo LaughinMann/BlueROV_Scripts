8dda68b5b8b33ba8babb8b47ad0172dffe636764
Kelvin Dhoman <kelvin.dhoman@gmail.com>
2023-02-14 15:28:14 -0500

    Create qgc_sender.py

---
+++ qgc_sender.py
@@ -0,0 +1,56 @@
+import random
+import time
+from pymavlink import mavutil
+
+# Default ip and port for QGroundControl
+qgc_ip = "localhost"
+qgc_port = 14550
+
+# Establish a connection to QGroundControl
+mavudp = mavutil.mavlink_connection("udpout:{}:{}".format(qgc_ip, qgc_port), source_system=1, source_component=1)
+print("QGC_Modem_Link: Sending Data for QGroundControl on {}:{}".format(qgc_ip, qgc_port))
+
+# Continuously send heartbeat messages to QGroundControl
+while True:
+    # Create a MAVLink heartbeat message
+    msg_heartbeat = mavutil.mavlink.MAVLink_heartbeat_message(
+        custom_mode=19,
+        system_status=5,
+        base_mode=81,
+        autopilot=3,
+        type=12,
+        mavlink_version=3
+    )
+
+    # Pack the message into a byte stream
+    msg_heartbeat.pack(mavudp.mav)
+
+    print("Sending HEARTBEAT Message to QGroundControl: ", msg_heartbeat)
+    # Send the message over the UDP connection
+    mavudp.mav.send(msg_heartbeat)
+
+    # Create a MAVLink system status message
+    msg_system_status = mavutil.mavlink.MAVLink_sys_status_message(
+        onboard_control_sensors_present=321977359,
+        onboard_control_sensors_enabled=304126991,
+        onboard_control_sensors_health=321952783,
+        load=434,
+        voltage_battery=random.randint(10000, 15000),
+        current_battery=46,
+        battery_remaining=99,
+        drop_rate_comm=0,
+        errors_comm=0,
+        errors_count1=0,
+        errors_count2=0,
+        errors_count3=0,
+        errors_count4=0)
+
+    # Pack the message into a byte stream
+    msg_system_status.pack(mavudp.mav)
+
+    print("Sending SYS_STATUS Message to QGroundControl: ", msg_system_status)
+    # Send the message over the UDP connection
+    mavudp.mav.send(msg_system_status)
+
+    # Wait for a short period before sending the next set of messages
+    time.sleep(1)
